8b46646a27893d6481a253652c3c686716dfd5ca
h1nee <infinitely.51white@inbox.ru>
2022-10-28 04:33:03 +0400

    end

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -26,11 +27,47 @@ namespace ExamTest2
     /// </summary>
     public partial class MainWindow : Window
     {
+        Capch capch = null;
+
+        public List<Capch> capches = new List<Capch>
+        {
+            new Capch {IsTrue="153" , path = "/ImageCapch/153.png"} ,
+            new Capch {IsTrue="197" , path = "/ImageCapch/197.png"} ,
+            new Capch {IsTrue="351" , path = "/ImageCapch/351.png"} ,
+        };
+
         public MainWindow()
         {
             InitializeComponent();
+            this.Loaded += MainWindow_Loaded;
+        }
+        private void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            RandomCapch();
         }
 
+        private void RandomCapch()
+        {
+            Random random = new Random();
+            var newcapch = capches[random.Next(capches.Count)];
+
+            if(capch == null)
+            {
+                capch = newcapch;
+                imageCapch.Source = new BitmapImage(new Uri(capch.path, UriKind.Relative));
+                return;
+            }
+            if(capch.IsTrue != newcapch.IsTrue)
+            {
+                capch = newcapch;
+                imageCapch.Source = new BitmapImage(new Uri(capch.path, UriKind.Relative));
+                return;
+            }
+            if(capch.IsTrue == newcapch.IsTrue)
+            {
+                RandomCapch();
+            }
+        }
         private void btnJoin_Click(object sender, RoutedEventArgs e)
         {
             if (string.IsNullOrEmpty(tbLogin.Text))
@@ -43,61 +80,85 @@ namespace ExamTest2
                 MessageBox.Show("Введите пароль");
                 return;
             }
-            //Authorization();
-            if (tbLogin.Text == "admin" && pbPassword.Password == "admin1")
+            if (tbCapch.Text != capch.IsTrue)
             {
-                MyForms.AdminWindow adminWindow = new MyForms.AdminWindow();
-                adminWindow.Show();
-                Close();
+                MessageBox.Show("Вы ввели неправильную капчу! - Попробуйте снова через 5 секунд");
+                for (int i = 5; i > 0; i--)
+                {
+                    this.Title = $"Блокровка {i} секунд";
+                    Thread.Sleep(1000);
+
+                }
+                RandomCapch();
+                tbCapch.Clear();
+                return;
+
             }
-            else
+            Authorization();
+            ////////if (tbLogin.Text == "admin" && pbPassword.Password == "admin1")
+            ////////{
+            ////////    MyForms.AdminWindow adminWindow = new MyForms.AdminWindow();
+            ////////    adminWindow.Show();
+            ////////    Close();
+            ////////}
+            ////////else
+            ////////{
+            ////////    MessageBox.Show("Неправильный пароль или логин");
+            ////////}
+        }
+        private void Authorization()
+        {
+            try
             {
-                MessageBox.Show("Неправильный пароль или логин");
+                DataBase.MyContext myContext = new DataBase.MyContext();
+                if (myContext.users.Any(x => x.Login == tbLogin.Text && x.Password == pbPassword.Password))
+                {
+                    var user = myContext.users.SingleOrDefault(x => x.Login == tbLogin.Text && x.Password == pbPassword.Password);
+
+                    switch (user.Role)
+                    {
+                        case "admin":
+                            MessageBox.Show($"{user.UserName}, добро пожаловать!");
+                            MyForms.AdminWindow adminWindow = new MyForms.AdminWindow();
+                            adminWindow.Show();
+                            Close();
+                            break;
+
+                        case "client":
+                            MessageBox.Show($"{user.UserName}, добро пожаловать!");
+                            MyForms.ClientWindow clientWindow = new MyForms.ClientWindow();
+                            clientWindow.Show();
+                            Close();
+                            break;
+
+                        case "Menager":
+                            MessageBox.Show($"{user.UserName}, добро пожаловать!");
+                            UsersForms.MenagerWindow menagerWindow = new UsersForms.MenagerWindow();
+                            menagerWindow.Show();
+                            Close();
+                            break;
+
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Неправильный логин или пароль");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Ошибка");
             }
         }
-        //private void Authorization()
-        //{
-        //    try
-        //    {
-        //        DataBase.MyContext myContext = new DataBase.MyContext();
-        //        if (myContext.users.Any(x => x.Login == tbLogin.Text && x.Password == pbPassword.Password))
-        //        {
-        //            var user = myContext.users.SingleOrDefault(x => x.Login == tbLogin.Text && x.Password == pbPassword.Password);
+        public class Capch
+        {
+            public string path { get; set; }
+            public string IsTrue { get; set; }
+        }
 
-        //            switch (user.Role)
-        //            {
-        //                case "admin":
-        //                    MessageBox.Show($"{user.UserName}, добро пожаловать!");
-        //                    MyForms.AdminWindow adminWindow = new MyForms.AdminWindow();
-        //                    adminWindow.Show();
-        //                    Close();
-        //                    break;
-
-        //                case "client":
-        //                    MessageBox.Show($"{user.UserName}, добро пожаловать!");
-        //                    MyForms.ClientWindow clientWindow = new MyForms.ClientWindow();
-        //                    clientWindow.Show();
-        //                    Close();
-        //                    break;
-
-        //                case "Menager":
-        //                    MessageBox.Show($"{user.UserName}, добро пожаловать!");
-        //                    UsersForms.MenagerWindow menagerWindow = new UsersForms.MenagerWindow();
-        //                    menagerWindow.Show();
-        //                    Close();
-        //                    break;
-
-        //            }
-        //        }
-        //        else
-        //        {
-        //            MessageBox.Show("Неправильный логин или пароль");
-        //        }
-        //    }
-        //    catch (Exception ex)
-        //    {
-        //        MessageBox.Show(ex.Message, "Ошибка");
-        //    }
-        //}
+        private void btnRefreshCapch_Click(object sender, RoutedEventArgs e)
+        {
+            RandomCapch();
+        }
     }
 }
---
+++ Migrations/20221027233209_test1.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ExamTest2.Migrations
+{
+    public partial class test1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "products",
+                columns: table => new
+                {
+                    ProductId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ProductName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Articul = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Manufacture = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Category = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Price = table.Column<double>(type: "float", nullable: false),
+                    Sale = table.Column<double>(type: "float", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_products", x => x.ProductId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "users",
+                columns: table => new
+                {
+                    UserId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UserName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Login = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Password = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Role = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_users", x => x.UserId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "products");
+
+            migrationBuilder.DropTable(
+                name: "users");
+        }
+    }
+}
